800c647c529467d69f17f00fbe20a02d3f67bcb9
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-12-10 12:30:59 +0100

    build: sets doc

---
+++ set.py
@@ -0,0 +1,12 @@
+# sets / conjuntos
+mi_set = {1, 2, 3, 4, 5, 6}
+
+print(f'Mi set completo: {mi_set}')
+
+mi_set.add(7)
+
+print(f'Set con nuevo elemento: {mi_set}')
+
+mi_set.remove(5)
+
+print(f'Mi set sin el ultimo elemento: {mi_set}')
