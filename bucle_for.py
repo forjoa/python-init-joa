aa529e0e62c6865b5508081d9832e69e3e7f234b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-12-12 16:57:41 +0100

    build: for explanation

---
+++ bucle_for.py
@@ -0,0 +1,11 @@
+lista_nombres = ["joaquin", "forky", "juan", "pepe"]
+
+# for para recorrer esta lista
+for nombre in lista_nombres:
+    print(nombre)
+    
+    
+# for con un rango
+for numero in range(25):
+    print(numero)
+
