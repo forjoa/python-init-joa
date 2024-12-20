d734e8a0ebe9fe556cf6e72b391e14f13c557335
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-12-20 12:03:39 +0100

    build: words counter

---
+++ ex.py
@@ -0,0 +1,13 @@
+palabras = ["sol", "estrella", "luz", "planeta", "galaxia"]
+contador = 0
+
+for palabra in palabras:
+    if len(palabra) > 4:
+        print(palabra)
+        contador = contador + 1 
+        
+print(f"Se encontraron {contador} palabras con más de 4 letras")
+        
+        
+
+
